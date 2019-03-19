c5a6d67c4f487f5b7595392323a8b0970c03df3d
fymmit <blomqvist.timo@gmail.com>
2019-03-19 18:35:56 +0200

    Add memepic search

--- functions/memepics2.js
+++ functions/memepics2.js
@@ -2,12 +2,22 @@ var request = require('request');
 
 let url = 'https://photos.timblo.us/'
 
-function meme(client, message) {
+function meme(client, message, tagstring) {
+    const tags = tagstring ? tagstring.split(' ') : null
     request.get(`${url}images`, (err, res, body) => {
         if (!err) {
             let images = JSON.parse(body);
-            let random = Math.floor((Math.random() * images.length));
-            let randomImage = images[random]
+            const filteredImages = !tags ? images : images.filter(image => {
+                const imageTags = image.tags.split(' ')
+                for (tag of tags) {
+                    if (imageTags.indexOf(tag) === -1) {
+                        return false;
+                    }
+                }
+                return true;
+            })
+            let random = Math.floor((Math.random() * filteredImages.length));
+            let randomImage = filteredImages[random]
             message.channel.send(`${url}${randomImage.name}`);
         }
     })
